6e599e9bf2ee10ac4c3842909f4c2bf9b778b1de
Bruno <brunocfaraco@hotmail.com.br>
2022-07-11 15:03:08 -0300

    realiazação do 1º teste do 1º requisito

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,4 +4,41 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
 
+  test('if fetchProducts is a function', () => {
+    const test = typeof(fetchProducts);
+    const expected = 'function';
+    expect(test).toEqual(expected);
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
 });
