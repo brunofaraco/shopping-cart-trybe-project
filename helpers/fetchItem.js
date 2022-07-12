3006008438a5f26a1fe093209098ec24edcc5e9b
Bruno <brunocfaraco@hotmail.com.br>
2022-07-11 22:46:56 -0300

    realização do 1º teste do 2º requisito

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (item) => {
+  try {
+    if (!item) throw new Error('You must provide and url');
+    const apiMercadoLivre = `https://api.mercadolibre.com/items/${item}`;
+    const response = await fetch(apiMercadoLivre);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,36 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  test('if fetchItem is a function', () => {
+    const tested = typeof(fetchItem);
+    const expected = 'function';
+    expect(tested).toEqual(expected);
+  });
+
+  // test('if "fetch" is called one time when you call "fetchItems("MLB1615760527")"', async () => {
+  //   expect.assertions(1);
+  //   await fetchItem('MLB1615760527');
+  //   expect(fetch).toHaveBeenCalledTimes(1);
+  // });
+
+  // test('if fetch URL is : https://api.mercadolibre.com/items/MLB1615760527', async () => {
+  //   expect.assertions(1);
+  //   await fetchItems('MLB1615760527');
+  //   const tested = fetch;
+  //   const expected = 'https://api.mercadolibre.com/items/MLB1615760527';
+  //   expect(tested).toHaveBeenCalledWith(expected);
+  // });
+  
+  // test('if fetchItems("MLB1615760527") returns the same that is into "item"', async () => {
+  //   expect.assertions(1)
+  //   const tested = await fetchItems('MLB1615760527');
+  //   expect(tested).toEqual(item);
+  // });
+
+  // test('if fetchItems() returns the expected error', async () => {
+  //   const tested = await fetchItems();
+  //   const expected = new Error('You must provide and url');
+  //   expect(tested).toEqual(expected);
+  // });
 });
